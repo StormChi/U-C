715e679eb484a05d1d6c28637729ab56973f74bb
StormChi <aifeng092@gmail.com>
2016-09-06 14:54:47 +0800

    rename

---
+++ Upenn/110_Java/Homework/hw00/MySketch.java
@@ -0,0 +1,8 @@
+public class MySketch {
+    public static void main(String[] args) {
+        // set the size of the window to 500 pixels by 500 pixels
+        StdDraw.setCanvasSize(500, 500);
+        
+        StdDraw.clear(StdDraw.BLACK);
+    }
+}
